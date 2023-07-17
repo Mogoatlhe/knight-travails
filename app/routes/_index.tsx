6fe850dc513f8c37e5b8b05f3b2cb12d90ac0ece
mogoatlhe <iam@ofent.se>
2023-07-17 15:29:33 +0200

    Add project description

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -3,7 +3,11 @@ import type { V2_MetaFunction } from "@remix-run/node";
 export const meta: V2_MetaFunction = () => {
   return [
     { title: "Knight Travails" },
-    { name: "description", content: "Welcome to Remix!" },
+    {
+      name: "description",
+      content:
+        "Find the shortest path a knight can take from one position on a chessboard to another.",
+    },
   ];
 };
 
